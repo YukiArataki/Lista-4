c2124ef25e7281ae2bff37a218c9b948102d0a16
Auto Logon <autologon@CSED.LAB>
2024-04-25 10:54:21 -0300

    Li

---
+++ Cenario01/src/br/edu/up/modelos/Livro.java
@@ -0,0 +1,47 @@
+package br.edu.up.modelos;
+
+public class Livro {
+
+    private String codigo, titulo, autor, isbn;
+    private int ano;
+
+    public Livro (String codigoLivro, String tituloLivro, String autores, int ano){
+        this.codigo = codigoLivro;
+        this.titulo = tituloLivro;
+        this.autor = autores;
+        this.ano = ano;
+    }
+
+    public String getCodigo(){
+        return this.codigo;
+    }
+
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+    public String getAutor(){
+        return this.autor;
+    }
+
+    public int getAno(){
+        return this.ano;
+    }
+
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor){
+        this.autor = autor; 
+    }
+
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+
+}
