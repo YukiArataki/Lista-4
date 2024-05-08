8699175200710d0806b0d30a74a17ee5dbc04ddd
Auto Logon <autologon@CSED.LAB>
2024-05-08 09:21:36 -0300

    mais

---
+++ Cenario04/src/br/edu/up/telas/Menu.java
@@ -0,0 +1,70 @@
+package br.edu.up.telas;
+import java.util.Scanner;
+import br.edu.up.controles.Estacionamento;
+
+public class Menu {
+
+    Scanner leitor = new Scanner(System.in);
+
+    public void mostrar(){
+        int opcao;
+        String msgMenu = "";
+        msgMenu += "Entre com opção desejada: \n";
+        msgMenu += "1 - Visualizar mapa de vagas\n"; 
+        msgMenu += "2 - Entrada de veículos\n";
+        msgMenu += "3 - Saída de veículos\n";
+        msgMenu += "4 - Relatório\n";
+        msgMenu += "5 - Sair\n";
+       
+        do{
+        
+        opcao = leitor.nextInt();
+        
+        switch(opcao){
+            case 1:
+                leitor.showMessageDialog(null,leitor.visualizarMapa(),"CIC Park",leitor.QUESTION_MESSAGE);
+                break;
+
+            case 2:
+                registrarVeiculo();
+                break;
+
+            case 3:
+                Saida();
+                break;
+
+            case 4:
+                leitor.showMessageDialog(null, leitor.exibirRelatorio());
+                break;
+
+            case 5:
+                break;
+                
+            default:
+                JOptionPane.showMessageDialog(null, "Opção Inválida!","Estacionamento",leitor.WARNING_MESSAGE); 
+        }
+        
+        }while(opcao!=5);
+    }  
+    public static void registrarVeiculo(){
+        int tmpTipo, numVaga;
+        int vagas = Integer.parseInt(leitor.showInputDialog("Qual  a quantidade de vagas"));
+        Estacionamento a[] = new Estacionamento[vagas];
+        String c="";
+
+        for(int i=0;i<vagas;i++) {
+            a[i]= new Estacionamento();
+            String marca = leitor.showInputDialog("Qual a marca do carro: "+i);
+            String modelo = leitor.showInputDialog("Qual o modelo do carro: "+i);
+            String placa = leitor.showInputDialog("Qual a placa do carro: "+i);
+            float entrada = Float.parseFloat(leitor.showInputDialog("Qual  a hora de entrada do carro "+i));
+            float saida = Float.parseFloat(leitor.showInputDialog("Qual  a hora de saida do carro "+i));
+            c = a[i].Criarcarro(marca, modelo, placa, entrada,saida);
+        }
+
+        for(int i=0;i<vagas;i++) {
+            leitor.showMessageDialog(null," O "+(i+1)+ " Carro é: " +c +"\n"+" A entrada foi as: "+a[i].getEntrada() +" horas "+ "\n" + " A hora de saida foi: "+a[i].getSaida()+ "horas"+"\n" +" O preço foi: R$:"+ a[i].getPreco() +"\n");
+        }
+    }
+
+    }
