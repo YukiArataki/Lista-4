c08db74f4a63f11b2305e5927f37d75d98f1b871
Auto Logon <autologon@CSED.LAB>
2024-05-24 09:28:14 -0300

    menos

--- Cenario01/src/br/edu/up/Programa.java
+++ Cenario01/src/br/edu/up/Programa.java
@@ -1,15 +1,31 @@
 package br.edu.up;
+import java.util.Scanner;
+
 import br.edu.up.modelos.Livro;
 
 public class  Programa{
-<<<<<<< HEAD
+
     public static void main(String[] args) {
-        
-        Livro harrypotter = new Livro("Harry Potter", "J.K Rowling");
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Qual é o código do livro ?");
+        String codigo = leitor.next();
+
+        System.out.println("Qual o titulo do livro ?");
+        String titulo = leitor.next();
+
+        System.out.println("Quem são os autores ?");
+        String autor = leitor.next();
+
+        System.out.println("Digite a ISBN do livro ");
+        String isbn = leitor.next();
+
+        System.out.println("Qual o ano de publicação do livro ?");
+        int ano = leitor.nextInt();
+
+        for (int i = 0; i < 2; i++) {
+            
+        }
     }
-=======
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    } 
->>>>>>> f5e1a54d29bcf876ef2e5a38fd0f12d571b58983
 }
